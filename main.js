88ad6cbb2f295daf192d75b76fcc694f1971be07
Andres Oliveira <andresoliveira@protonmail.com>
2024-07-03 10:51:50 -0300

    on Load Youtube top rated videos
    - Added youtube api request
    - Added params like max length and topRated videos
    - Added function on onload

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+const apiKey = "API KEY HERE"
+const apiURL = "https://www.googleapis.com/youtube/v3"
+
+const params = {
+  type: "video",
+  part: "snippet",
+  rate: "getRating",
+  maxResults: 20,
+}
+
 const hamburger = document.getElementById("hamburger")
 const closeSidebar = document.getElementById("close-sidebar")
 const sidebar = document.querySelector("sidebar")
+
+window.addEventListener('load', () => {
+  getTopVideos()
+})
+
+const getTopVideos = async () => {
+  const req = await fetch(`${apiURL}/search?key=${apiKey}&type=${params.type}&part=${params.part}&${params.rate}&maxResults=${params.maxResults}`)
+  const data = await req.json()
+  console.log(data.items)
+}
 
 const showSidebar = (e) => {
     e.preventDefault()
